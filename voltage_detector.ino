d543b940ec1cd184660e9157b2159ce535dccc46
nomath4u <nomath4u@gmail.com>
2013-11-24 16:29:10 -0800

    Rewrote most of it. Now will display the rounded value digit on seven segment display, need to test with source higher then 5v next

--- voltage_detector.ino
+++ voltage_detector.ino
@@ -1,42 +1,58 @@
-int sensorPin = A0;    // select the input pin for Vin
-/*Seven Segment display pins*/
-int a = 2; 
-int b = 3;
-int c = 4;
-int d = 5;
-int e = 6;
-int f = 7;
-int g = 8;
-int h = 9;
-int com = 10;
-int sensorValue = 0;  // variable to store the value coming from the sensor
+//#include <ShiftRegister595.h>
 
+
+//#include <SevenSegment.h>
+
+byte seven_seg_digits[10][7] = { { 1,1,1,1,1,1,0 },  // = 0
+                                 { 0,1,1,0,0,0,0 },  // = 1
+                                 { 1,1,0,1,1,0,1 },  // = 2
+                                 { 1,1,1,1,0,0,1 },  // = 3
+                                 { 0,1,1,0,0,1,1 },  // = 4
+                                 { 1,0,1,1,0,1,1 },  // = 5
+                                 { 1,0,1,1,1,1,1 },  // = 6
+                                 { 1,1,1,0,0,0,0 },  // = 7
+                                 { 1,1,1,1,1,1,1 },  // = 8
+                                 { 1,1,1,0,0,1,1 }   // = 9
+                                 };
+int sensorPin = A0;    // select the input pin for Vin
+int sensorValue = 0;  // variable to store the value coming from the sensor
+float fraction = 1023/5; //Max adc out is 1023 and 5V going in
+byte out;
 void setup() {
   // declare the Seven Segment Display Pins as an OUTPUT:
-  pinMode(a, OUTPUT);
-  pinMode(b, OUTPUT);
-  pinMode(c, OUTPUT);
-  pinMode(d, OUTPUT);
-  pinMode(e, OUTPUT);
-  pinMode(f, OUTPUT);
-  pinMode(g, OUTPUT);
-  pinMode(h, OUTPUT);
-  pinMode(com, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
   /*Start a Serial connection for testing*/
   //Serial.begin(9600);
   //Serial.println("Beginning Serial Output");
-  /*Set common pin to low*/
-  digitalWrite(com,LOW);
+  /*Set dot pin to low*/
+  digitalWrite(9,LOW);
 }
+
+void sevenSegWrite(byte digit) {
+  byte pin = 2;
+  for (byte segCount = 0; segCount < 7; ++segCount) {
+    digitalWrite(pin, seven_seg_digits[digit][segCount]);
+    ++pin;
+  }
+}
+
+byte convert(int val){
+  out = (val)/fraction;
+  return out;
+}
+  
 
 void loop() {
   sensorValue = analogRead(sensorPin);
-  //Serial.println(sensorValue);
-  if(sensorValue ==1023){
-      digitalWrite(f,HIGH);
-  }
-  else{
-      digitalWrite(f,LOW);
-  }
-  //delay(1000);  
+
+  sevenSegWrite(convert(sensorValue));
+  delay(10);
+
 }
